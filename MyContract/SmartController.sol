8d355b3ef57a32706055f709c8f850b5f1cb6a88
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2018-06-30 02:30:11 +0900

    Add files via upload

--- MyContract/SmartController.sol
+++ MyContract/SmartController.sol
@@ -5,29 +5,27 @@ contract SmartController {
     struct Controller {
     address add; // client의 주소
     uint endTime; // 이용 종료 시간
-    bool staus; // true알 때만 사용가능
+    bool status; // true 때만 사용가능
     }
 
-    address public owner; // 오너의 주소
-    address public iot; // IoT Device의 주소
+    address public owner; // 오너의 주소 (EOA)
+    address public iot; // IoT Device의 주소 (CA)
 
     mapping (uint => Controller) public controllers; // Controller 구조체를 담을 매핑
-    uint public numPaid; //결제 횟수
+    uint public numPaid; //결제 횟수 카운터 변수
 
     /// 오너의 권한 체크
     modifier onlyOwner() {
       require(msg.sender == owner);
       _;
     }
-
     /// IoT 장치 권한 체크
     modifier onlyIoT() {
       require(msg.sender == iot);
       _;
     }
-
     ///생성자, IoT장치의 주소를 매개변수로 받음
-    function SmartController(address _iot) {
+    function SmartController(address _iot) public {  // 접근지정자 꼭!
       // owner의 값에 이 계약을 생성한 계정 주소 대입
       owner = msg.sender;
       iot = _iot;
@@ -36,13 +34,12 @@ contract SmartController {
     //이더 지불 시, 호출되는 함수
     function payToSwitch() public payable {
       require(msg.value == 1000000000000000000); //1ETH가 아니면 종료
-
-      Controller c = controllers[numPaid++]; //Controller 생성
-      s.add = msg.sender;
-      s.endTime = now + 60; // 이용시간 1분, 원하는 시간 만큼 수정 가능
-      s.status = true;
+      //Controller 생성
+      Controller storage c = controllers[numPaid++]; //storage로 배열 생성
+      c.add = msg.sender;
+      c.endTime = now + 60; // 이용시간 1분, 원하는 시간 만큼 수정 가능
+      c.status = true;
     }
-
     /// 상태(staus)를 변경하는 함수, 이용 종료 시각에 호출
     // controllers의 키 값이 매개변수가 됨
     function updateStatus(uint _index) public onlyIoT {
@@ -54,13 +51,10 @@ contract SmartController {
 
       controllers[_index].status = false; //상태변경
     }
-
     // 지불된 이더를 인출하는 함수
     function withdrawFunds() public onlyOwner {
-    	if (!owner.send(this.balance))
-    		throw;
+    	require (owner.send(address(this).balance));
     }
-
     // 계약을 소멸시키는 함수
     function kill() public onlyOwner {
       selfdestruct(owner);
